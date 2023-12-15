1acb21c8dbd6c0e3857f4c430b8d3e2ca7c0e052
abuddies <91999625+abuddies0@users.noreply.github.com>
2023-12-14 19:17:45 -0500

    Added Structure and Organization
    1) Constants.java stores any constant values such as port numbers, size measurements, or control variables.
    2) RobotMap.java stores any variables (and subsystems) that refer to a physical part of the robot.
    3) The subsystems folder contains each individual subsystem which will contain everything about the motors and commands unique to each one.

---
+++ MockSteamworksBot/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,8 @@
+package frc.robot;
+
+
+public class Constants {
+    
+    public static final double EXAMPLE = 5;     // Just an example! Make sure to ALWAYS comment the variable here! If it's a port number, put what type of port! PWM, DIO, or CAN!
+    
+}
---
+++ MockSteamworksBot/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,7 @@
+package frc.robot;
+
+public class RobotMap {
+    
+    // Example: public static CANSparkMax m1 = new CanSparkMax(Constants.PORT);
+
+}
